8ebd07844f8c2439a98222f801f67880640507b9
niujianlong <jl@shfangyan.cn>
2017-07-08 18:03:37 +0800

    backup

--- todi/src/app/can/app_can.c
+++ todi/src/app/can/app_can.c
@@ -60,6 +60,8 @@ static U8 msg_buf_num = 0;	//record msg buf mum
 can_msg_t can_msg[ID_PROCESS_ALL] = { 0 };
 static pCanAnalyse can_rx_handle[ID_RECV_NUM_ALL] = { NULL };
 U32 can_rx_msg_id_ads[ID_RECV_NUM_ALL] = { 0 };
+U16 BAT_Cell_Voltage[400] = {0};
+U8  BAT_Cell_Temperature[128] = {0};
 
 can_signal_t can0 = { 0 };
 #if 1
@@ -242,7 +244,14 @@ static void can_id_7E7_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_100017EF_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_100017EF[0], (U8*) &msg->data[0], msg->dlc);
+		VCU_TM_Speed_percent = (unsigned char) msg->data[0];
+        VCU_TM_Brake_percent = (unsigned char) msg->data[1];
+        VCU_Status_Flag1.byte = (unsigned char) msg->data[2];
+        VCU_Gear = msg->data[3]&0x0F;
+        VCU_Mode.byte = msg->data[3] & 0xF0;
+        VCU_Life = msg->data[4]&0x0F;
+        VCU_Status_Flag2.byte = msg->data[4];
+        VCU_Code = msg->data[7];
 		can_id_100017EF_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -254,7 +263,12 @@ static void can_id_100017EF_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_1801FBEF_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_1801FBEF[0], (U8*) &msg->data[0], msg->dlc);
+		TM_Number = msg->data[0];
+        TM_Serial = msg->data[1];
+        TM_WorkStatus = msg->data[2];
+        TM_Control_Temp = msg->data[3];
+        TM_Feedback_RPM = (unsigned char) msg->data[4]+((unsigned int) msg->data[5] << 8);
+        TM_Feedback_NM = (unsigned char) msg->data[6]+((unsigned int) msg->data[7] << 8);
 		can_id_1801FBEF_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -266,7 +280,9 @@ static void can_id_1801FBEF_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_1811FBEF_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_1811FBEF[0], (U8*) &msg->data[0], msg->dlc);
+		TM_Temp = msg->data[0];
+        TM_Voltage_DC = (unsigned char) msg->data[1]+((unsigned int) msg->data[2] << 8);
+        TM_Current_DC = (unsigned char) msg->data[3]+((unsigned int) msg->data[4] << 8);
 		can_id_1811FBEF_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -276,9 +292,18 @@ static void can_id_1811FBEF_analyse(can_msg_t *msg, can_pro_way_e way) {
 	}
 }
 static void can_id_18FF08F2_analyse(can_msg_t *msg, can_pro_way_e way) {
+	DATA_DOUBLE CAN_DOUBLE;
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF08F2[0], (U8*) &msg->data[0], msg->dlc);
+		IRM_Ohm_Positive = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8)+((unsigned long) msg->data[2] << 16);
+		IRM_Ohm_Negative = (unsigned char) msg->data[3]+((unsigned int) msg->data[4] << 8)+((unsigned long) msg->data[5] << 16);
+
+		CAN_DOUBLE.byte = msg->data[6];
+		IRM_Fault_Level = CAN_DOUBLE.bit1;
+		IRM_Insulation_Level = CAN_DOUBLE.bit2;
+
+		IRM_Life = msg->data[7];
+
 		can_id_18FF08F2_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -290,7 +315,13 @@ static void can_id_18FF08F2_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_10F8159E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_10F8159E[0], (U8*) &msg->data[0], msg->dlc);
+		BMS_Mode = msg->data[0]&0x07;
+        BMS_Status = (msg->data[0] >> 6)&0x03;
+        BAT_Temp_Average = msg->data[1];
+        BMS_SOC = msg->data[2];
+        BMS_Current = (unsigned char) msg->data[3]+((unsigned int) msg->data[4] << 8);
+        BMS_Voltage = (unsigned char) msg->data[5]+((unsigned int) msg->data[6] << 8);
+        BMS_Kt = (msg->data[7] >> 4)&0x0F;
 		can_id_10F8159E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -302,7 +333,11 @@ static void can_id_10F8159E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_10F8169E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_10F8169E[0], (U8*) &msg->data[0], msg->dlc);
+		BAT_Temp_H_Limit = msg->data[0];
+        BAT_Temp_L_Limit = msg->data[1];
+        BMS_SOC_L_Limit = msg->data[2];
+        BAT_Current_Discharge_Limit = (unsigned char) msg->data[3]+((unsigned int) msg->data[4] << 8);
+        BAT_Current_Charge_Limit = (unsigned char) msg->data[5]+((unsigned int) msg->data[6] << 8);
 		can_id_10F8169E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -314,7 +349,12 @@ static void can_id_10F8169E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18F8179E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18F8179E[0], (U8*) &msg->data[0], msg->dlc);
+		BAT_Temp_L = msg->data[0];
+		BAT_Temp_L_Number = msg->data[1];
+		BAT_Temp_L_Case = msg->data[2];
+		BAT_Temp_H = msg->data[3];
+		BAT_Temp_H_Number = msg->data[4];
+		BAT_Temp_H_Case = msg->data[5];
 		can_id_18F8179E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -326,7 +366,16 @@ static void can_id_18F8179E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18F8189E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18F8189E[0], (U8*) &msg->data[0], msg->dlc);
+		BAT_Voltage_Fault = msg->data[0]&0x0F;
+		BAT_Single_Fault = (msg->data[0] >> 4)&0x0F;
+		BAT_Temp_Fault = msg->data[1]&0x3F;
+		BAT_Insulation_Fault = (msg->data[1] >> 6)&0x3F;
+		BAT_Consistency_Fault = msg->data[2]&0x03;
+		BAT_SOC_Fault = (msg->data[2] >> 2)&0x3F;
+		BAT_Current_Fault = msg->data[3]&0x0F;
+		BAT_Lock_Fault = (msg->data[3] >> 4)&0x03;
+		BMS_Comm_Fault = (msg->data[3] >> 6)&0x03;
+		BMS_System_Fault = msg->data[4]&0x01;
 		can_id_18F8189E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -338,7 +387,11 @@ static void can_id_18F8189E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18F8199E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18F8199E[0], (U8*) &msg->data[0], msg->dlc);
+		BMS_Ohm = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		BMS_Number = msg->data[2];
+		BMS_Temp_Number = msg->data[3];
+		BMS_System_Unit = msg->data[4];
+		BMS_System_Number = msg->data[5];
 		can_id_18F8199E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -350,7 +403,12 @@ static void can_id_18F8199E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18F81A9E_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18F81A9E[0], (U8*) &msg->data[0], msg->dlc);
+		BAT_Single_L = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		BAT_Single_L_Number = msg->data[2];
+		BAT_Single_L_Case = msg->data[3];
+		BAT_Single_H = (unsigned char) msg->data[4]+((unsigned int) msg->data[5] << 8);
+		BAT_Single_H_Number = msg->data[6];
+		BAT_Single_H_Case = msg->data[7];
 		can_id_18F81A9E_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -362,7 +420,16 @@ static void can_id_18F81A9E_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18FF12F7_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF12F7[0], (U8*) &msg->data[0], msg->dlc);
+		DCDC_Voltage = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		DCDC_Current = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+		DCDC_Status = msg->data[4]&0x03;
+		DCDC_Output_Cut = (msg->data[4] >> 2)&0x01;
+		DCDC_intput_Cut = (msg->data[4] >> 3)&0x01;
+		DCDC_Fault_Code = msg->data[5];
+		DCDC_Level_Fault = msg->data[6]&0x03;
+		DCDC_Temp_Warn = (msg->data[6] >> 2)&0x01;
+		DCDC_Fault_Warn = (msg->data[6] >> 3)&0x01;
+		DCDC_Temp = msg->data[7];
 		can_id_18FF12F7_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -374,7 +441,16 @@ static void can_id_18FF12F7_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18FF0AF8_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF0AF8[0], (U8*) &msg->data[0], msg->dlc);
+		OILDCAC_U_Voltage = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		OILDCAC_U_Current = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+		OILDCAC_Status = msg->data[4]&0x03;
+		OILDCAC_Output_Cut = (msg->data[4] >> 2)&0x01;
+		OILDCAC_intput_Cut = (msg->data[4] >> 3)&0x01;
+		OILDCAC_Fault_Code = msg->data[5];
+		OILDCAC_Level_Fault = msg->data[6]&0x03;
+		OILDCAC_Temp_Warn = (msg->data[6] >> 2)&0x01;
+		OILDCAC_Fault_Warn = (msg->data[6] >> 3)&0x01;
+		OILDCAC_Temp = msg->data[7];
 		can_id_18FF0AF8_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -386,7 +462,10 @@ static void can_id_18FF0AF8_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18FF0BF8_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF0BF8[0], (U8*) &msg->data[0], msg->dlc);
+		OILDCAC_V_Voltage = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		OILDCAC_V_Current = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+		OILDCAC_W_Voltage = (unsigned char) msg->data[4]+((unsigned int) msg->data[5] << 8);
+		OILDCAC_W_Current = (unsigned char) msg->data[6]+((unsigned int) msg->data[7] << 8);
 		can_id_18FF0BF8_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -398,7 +477,16 @@ static void can_id_18FF0BF8_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18FF0CF9_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF0CF9[0], (U8*) &msg->data[0], msg->dlc);
+		AIRDCAC_U_Voltage = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		AIRDCAC_U_Current = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+		AIRDCAC_Status = msg->data[4]&0x03;
+		AIRDCAC_Output_Cut = (msg->data[4] >> 2)&0x01;
+		AIRDCAC_intput_Cut = (msg->data[4] >> 3)&0x01;
+		AIRDCAC_Fault_Code = msg->data[5];
+		AIRDCAC_Level_Fault = msg->data[6]&0x03;
+		AIRDCAC_Temp_Warn = (msg->data[6] >> 2)&0x01;
+		AIRDCAC_Fault_Warn = (msg->data[6] >> 3)&0x01;
+		AIRDCAC_Temp = msg->data[7];
 		can_id_18FF0CF9_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -410,7 +498,10 @@ static void can_id_18FF0CF9_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_18FF0DF9_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_18FF0DF9[0], (U8*) &msg->data[0], msg->dlc);
+		AIRDCAC_V_Voltage = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+		AIRDCAC_V_Current = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+		AIRDCAC_W_Voltage = (unsigned char) msg->data[4]+((unsigned int) msg->data[5] << 8);
+		AIRDCAC_W_Current = (unsigned char) msg->data[6]+((unsigned int) msg->data[7] << 8);
 		can_id_18FF0DF9_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -422,7 +513,13 @@ static void can_id_18FF0DF9_analyse(can_msg_t *msg, can_pro_way_e way) {
 static void can_id_1801EFA9_analyse(can_msg_t *msg, can_pro_way_e way) {
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_1801EFA9[0], (U8*) &msg->data[0], msg->dlc);
+		ICS01_Status1.byte = msg->data[0];
+		ICS01_Status2.byte = msg->data[1];
+		ICS01_Status3.byte = msg->data[2];
+		ICS01_Status4.byte = msg->data[3];
+		ICS01_Voltage_Front = (unsigned char) msg->data[4]+((unsigned int) (msg->data[5]&0x0F) << 8);
+		ICS01_Voltage_Rear = (unsigned char) (msg->data[5] >> 4)+((unsigned int) msg->data[6] << 4);
+		ICS01_LIFE = msg->data[7];
 		can_id_1801EFA9_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
@@ -432,9 +529,30 @@ static void can_id_1801EFA9_analyse(can_msg_t *msg, can_pro_way_e way) {
 	}
 }
 static void can_id_104C1000_analyse(can_msg_t *msg, can_pro_way_e way) {
+	U8 i  = 0;
 	switch (way) {
 	case CAN_PARSE:
-		data_copy((U8*) &can0.ID_104C1000[0], (U8*) &msg->data[0], msg->dlc);
+		for (i = 0; i < 100; i++) {
+            if (msg->id == (0x104C19A4 + i)) {
+                BAT_Cell_Voltage[i * 4] = (unsigned char) msg->data[0]+((unsigned int) msg->data[1] << 8);
+                BAT_Cell_Voltage[(i * 4) + 1] = (unsigned char) msg->data[2]+((unsigned int) msg->data[3] << 8);
+                BAT_Cell_Voltage[(i * 4) + 2] = (unsigned char) msg->data[4]+((unsigned int) msg->data[5] << 8);
+                BAT_Cell_Voltage[(i * 4) + 3] = (unsigned char) msg->data[6]+((unsigned int) msg->data[7] << 8);
+            }
+        }
+        for (i = 0; i < 16; i++) {
+            if (msg->id == (0x104C1A18 + i)) {
+                BAT_Cell_Temperature[i * 8] = (unsigned char) msg->data[0];
+                BAT_Cell_Temperature[(i * 8) + 1] = (unsigned char) msg->data[1];
+                BAT_Cell_Temperature[(i * 8) + 2] = (unsigned char) msg->data[2];
+                BAT_Cell_Temperature[(i * 8) + 3] = (unsigned char) msg->data[3];
+                BAT_Cell_Temperature[(i * 8) + 4] = (unsigned char) msg->data[4];
+                BAT_Cell_Temperature[(i * 8) + 5] = (unsigned char) msg->data[5];
+                BAT_Cell_Temperature[(i * 8) + 6] = (unsigned char) msg->data[6];
+                BAT_Cell_Temperature[(i * 8) + 7] = (unsigned char) msg->data[7];
+            }
+        }
+		//data_copy((U8*) &can0.ID_104C1000[0], (U8*) &msg->data[0], msg->dlc);
 		can_id_104C1000_lost_timecnt = 0;
 		break;
 	case CAN_LOST:
--- todi/src/app/can/app_can.h
+++ todi/src/app/can/app_can.h
@@ -7,6 +7,34 @@
 
 #define		CAN_LOST_TIME			25
 
+
+typedef union {
+
+    struct {
+        unsigned bit1 : 2;
+        unsigned bit2 : 2;
+        unsigned bit3 : 2;
+        unsigned bit4 : 2;
+    };
+    unsigned char byte;
+} DATA_DOUBLE;
+
+typedef union {
+
+    struct {
+        unsigned bit1 : 1;
+        unsigned bit2 : 1;
+        unsigned bit3 : 1;
+        unsigned bit4 : 1;
+        unsigned bit5 : 1;
+        unsigned bit6 : 1;
+        unsigned bit7 : 1;
+        unsigned bit8 : 1;
+    };
+    unsigned char byte;
+} DATA_BIT;
+
+
 typedef enum
 {
 	ID_100017EF, //VCU2ICU01
@@ -75,9 +103,172 @@ typedef struct
 	U8 ID_18FF0CF9[8]; //AIRDCAC21
 	U8 ID_18FF0DF9[8]; //AIRDCAC22
 	U8 ID_1801EFA9[8]; //ICS01
-	U8 ID_104C1000[8]; //BATTERY V and temp
+	//U8 ID_104C1000[8]; //BATTERY V and temp
 
 } can_signal_t;
+
+
+
+/*******************分包处理*****************/
+//VCU
+unsigned int  VCU_TM_NM = 5000; //TM目标扭矩
+unsigned int  VCU_TM_RPM = 15000; //TM目标转速
+
+unsigned char VCU_TM_Brake_percent; //制动踏板开度
+unsigned char VCU_TM_Speed_percent; //加速踏板开度
+DATA_BIT VCU_Status_Flag1; //状态1
+unsigned char VCU_Gear; //档位
+DATA_BIT VCU_Mode; //整车模式
+unsigned char VCU_Life; //整车控制器LIFE
+DATA_BIT VCU_Status_Flag2;
+unsigned char VCU_Code; //整车故障码
+// VCU2TERMINAL_MOTOR01
+unsigned char TM_Number; //电机个数
+unsigned char TM_Serial; //电机序号
+unsigned char TM_WorkStatus; //TM电机工作状态
+unsigned char TM_Control_Temp = 40; //TM电机控制器温度
+unsigned int  TM_Feedback_RPM = 20000; //TM电机反馈转速
+unsigned int  TM_Feedback_NM = 20000; //TM电机反馈扭矩
+// VCU2TERMINAL_MOTOR02
+unsigned char TM_Temp = 40; //TM电机温度
+unsigned int  TM_Voltage_DC; //TM电机直流电压
+unsigned int  TM_Current_DC = 10000; //TM电机直流电流
+/*
+//MCU01
+unsigned int  TM_Feedback_RPM=20000;//TM电机反馈转速
+unsigned int  TM_Feedback_NM=10000;//TM电机反馈扭矩
+unsigned int  TM_Current_AC;//TM电机交流电流
+unsigned char TM_StatusFeedback;//TM电机状态反馈
+unsigned char TM_WorkStatus;//TM电机工作状态
+unsigned char TM_LIFE;//TM电机LIFE
+//MCU02
+unsigned char TM_Temp=40;//TM电机温度
+unsigned char TM_Control_Temp=40;//TM电机控制器温度
+unsigned int  TM_Torque_Limit_Up;//电机扭矩上限
+unsigned int  TM_Torque_Limit_Down;//电机扭矩下限
+DATA_BIT      TM_Fault;//TM电机故障等级
+unsigned char TM_Status;//TM电机状态
+DATA_BIT      TM_Status_Flag1;//电机状态1
+//MCU03
+unsigned int  TM_Code;//TM电机故障码
+unsigned int  TM_Current_DC=20000;//TM电机直流电流
+unsigned int  TM_Voltage_DC;//TM电机直流电压
+ */
+//IRM01
+unsigned long IRM_Ohm_Positive; //总正绝缘阻值 
+unsigned long IRM_Ohm_Negative; //总负绝缘阻值 
+unsigned char IRM_Fault_Level; //设备故障等级
+unsigned char IRM_Insulation_Level; //绝缘等级
+unsigned char IRM_Life; //绝缘LIFE
+
+/*unsigned char VCU_Kt; //主接触器控制命令
+unsigned char VCU_ON; //钥匙下电状态
+unsigned char VCU_Order; //故障处理命令
+unsigned int  VCU_Voltage; //电机控制器电压
+ */
+
+unsigned char BMS_Mode; //电池充放电模式
+unsigned char BMS_Status; //电池状态
+unsigned char BAT_Temp_Average = 40; //电池平均温度
+unsigned char BMS_SOC; //SOC
+unsigned int  BMS_Current = 32000; //电池系统电流
+unsigned int  BMS_Voltage; //电池系统内总电压
+unsigned char BMS_Kt; //高压继电器状态
+
+unsigned char BAT_Temp_H_Limit = 40; //最高允许电池单体温度
+unsigned char BAT_Temp_L_Limit = 40; //最低允许电池单体温度
+unsigned char BMS_SOC_L_Limit; //最低允许SOC值
+unsigned int  BAT_Current_Discharge_Limit = 32000; //最高允许放电电流
+unsigned int  BAT_Current_Charge_Limit = 32000; //最高允许充电电流
+
+unsigned char BAT_Temp_L = 40; //电池单体最低温度
+unsigned char BAT_Temp_L_Number; //电池单体低温度序号
+unsigned char BAT_Temp_L_Case; //电池单体低温度箱号
+unsigned char BAT_Temp_H = 40; //电池单体最高温度
+unsigned char BAT_Temp_H_Number; //电池单体高温度序号
+unsigned char BAT_Temp_H_Case; //电池单体高温度箱号
+
+unsigned char BAT_Voltage_Fault; //总压故障
+unsigned char BAT_Single_Fault; //单压故障
+unsigned char BAT_Temp_Fault; //温度故障
+unsigned char BAT_Insulation_Fault; //绝缘故障
+unsigned char BAT_Consistency_Fault; //电池一致性故障
+unsigned char BAT_SOC_Fault; //SOC故障
+unsigned char BAT_Current_Fault; //电流故障
+unsigned char BAT_Lock_Fault; //高压互锁故障
+unsigned char BMS_Comm_Fault; //BMS通讯故障
+unsigned char BMS_System_Fault; //BMS系统故障
+
+unsigned int  BMS_Ohm; //绝缘阻值 
+unsigned char BMS_Number; //单体电压数量
+unsigned char BMS_Temp_Number; //单体温度数量
+unsigned char BMS_System_Unit; //系统数量
+unsigned char BMS_System_Number; //系统号
+
+unsigned int  BAT_Single_L; //电池单体低电压
+unsigned char BAT_Single_L_Number; //电池单体低电压位置
+unsigned char BAT_Single_L_Case; //电池单体低电压箱号
+
+unsigned int  BAT_Single_H; //电池单体低电压
+unsigned char BAT_Single_H_Number; //电池单体低电压位置
+unsigned char BAT_Single_H_Case; //电池单体低电压箱号
+
+
+
+unsigned int  DCDC_Voltage; //DCDC输出电压
+unsigned int  DCDC_Current; //DCDC输出电流
+unsigned char DCDC_Status; //DCDC工作状态
+unsigned char DCDC_Output_Cut; //DCDC 输出切断
+unsigned char DCDC_intput_Cut; //DCDC 输入切断
+unsigned char DCDC_Fault_Code; //DCDC故障码
+unsigned char DCDC_Level_Fault; //DCDC故障等级
+unsigned char DCDC_Temp_Warn; //DCDC温度报警
+unsigned char DCDC_Fault_Warn; //DCDC故障报警
+unsigned char DCDC_Temp = 40; //DCDC模块温度
+
+unsigned int  OILDCAC_U_Voltage; //DCAC输出U电压
+unsigned int  OILDCAC_U_Current; //DCAC输出U电流
+unsigned char OILDCAC_Status; //DCAC工作状态
+unsigned char OILDCAC_Output_Cut; //DCAC 输出切断
+unsigned char OILDCAC_intput_Cut; //DCAC 输入切断
+unsigned char OILDCAC_Fault_Code; //DCAC故障代码
+unsigned char OILDCAC_Level_Fault; //DCAC故障等级
+unsigned char OILDCAC_Temp_Warn; //DCDC温度报警
+unsigned char OILDCAC_Fault_Warn; //DCDC故障报警
+unsigned char OILDCAC_Temp = 40; //DCAC模块温度
+
+unsigned int  OILDCAC_V_Voltage; //DCAC输出V电压
+unsigned int  OILDCAC_V_Current; //DCAC输出V电流
+unsigned int  OILDCAC_W_Voltage; //DCAC输出W电压
+unsigned int  OILDCAC_W_Current; //DCAC输出W电流
+
+unsigned int  AIRDCAC_U_Voltage; //DCAC输出U电压
+unsigned int  AIRDCAC_U_Current; //DCAC输出U电流
+unsigned char AIRDCAC_Status; //DCAC工作状态
+unsigned char AIRDCAC_Output_Cut; //DCAC 输出切断
+unsigned char AIRDCAC_intput_Cut; //DCAC 输入切断
+unsigned char AIRDCAC_Fault_Code; //DCAC故障代码
+unsigned char AIRDCAC_Level_Fault; //DCAC故障等级
+unsigned char AIRDCAC_Temp_Warn; //DCDC温度报警
+unsigned char AIRDCAC_Fault_Warn; //DCDC故障报警
+unsigned char AIRDCAC_Temp = 40; //DCAC模块温度
+
+unsigned int  AIRDCAC_V_Voltage; //DCAC输出V电压
+unsigned int  AIRDCAC_V_Current; //DCAC输出V电流
+unsigned int  AIRDCAC_W_Voltage; //DCAC输出W电压
+unsigned int  AIRDCAC_W_Current; //DCAC输出W电流
+
+DATA_DOUBLE ICS01_Status1; //高压采集状态1
+DATA_DOUBLE ICS01_Status2; //高压采集状态2
+DATA_DOUBLE ICS01_Status3; //高压采集状态3
+DATA_DOUBLE ICS01_Status4; //高压采集状态4
+
+unsigned int  ICS01_Voltage_Front; //前端电压
+unsigned int  ICS01_Voltage_Rear; //后端电压
+unsigned char ICS01_LIFE; //LIFE
+
+
+
 
 /*******************************************************************************/
 typedef void (*pCanAnalyse)(can_msg_t *msg, can_pro_way_e way);
@@ -107,6 +298,10 @@ extern U16 can_id_18FF0CF9_lost_timecnt;
 extern U16 can_id_18FF0DF9_lost_timecnt;
 extern U16 can_id_1801EFA9_lost_timecnt;
 extern U16 can_id_104C1000_lost_timecnt;
+
+//bat vol and temp
+extern U16  BAT_Cell_Voltage[400];
+extern U8   BAT_Cell_Temperature[128];
 
 
 #endif
